4835b290f47d2121dc3dcab77d0d7814409261e1
shumes06 <116228972+shumes06@users.noreply.github.com>
2023-03-30 17:09:56 -0400

    Created a simple drive train code. It functions as a tank drive right now, but will be expanded to a full mecanum soon.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spencerBotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spencerBotTest.java
@@ -1,24 +1,62 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@Autonomous
-@Disabled
-
+@TeleOp
 public class spencerBotTest extends LinearOpMode{
-    private Blinker control_Hub;
-    public void blinkerSetup(){
-        control_Hub = hardwareMap.get(Blinker.class, "Control Hub");
-    }
-    public int color = 0;
+    private DcMotor rightFront, rightBack, leftFront, leftBack;
+    public boolean isMotorSetup = false;
+
     @Override
     public void runOpMode(){
-        while (opModeIsActive()){
-            control_Hub.setConstant(color);
-            color +=1;
+    motorSetup();
+    telemetry.addData("Motor Status", isMotorSetup);
+    telemetry.update();
+    waitForStart();
+    operation();
+    }
+
+    public boolean motorSetup(){
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        return isMotorSetup;
+    }
+    public void drivePower(){
+        double translX = gamepad1.left_stick_x;
+        double translY = -gamepad1.left_stick_y;
+        double rot = gamepad1.right_stick_x;
+
+        leftFront.setPower(translY + rot);
+        leftBack.setPower(translY + rot);
+
+        rightFront.setPower(translY - rot);
+        rightBack.setPower(translY - rot);
+    }
+    public void operation(){
+        while(opModeIsActive()){
+            drivePower();
+            telemetry.addData("Running?", opModeIsActive());
+            telemetry.update();
         }
+        telemetry.addData("Running?", opModeIsActive());
+        telemetry.update();
     }
 }
