3eebdf2896bbfb8b81940127819267fe33217b54
shumes06 <116228972+shumes06@users.noreply.github.com>
2023-04-01 10:08:16 -0400

    This commit adds field oriented control. The heading can be reset with the option button on gamepad 1. Untested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spencerBotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spencerBotTest.java
@@ -1,17 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 public class spencerBotTest extends LinearOpMode{
     private DcMotor rightFront, rightBack, leftFront, leftBack;
-
+    private IMU imu;
     @Override
     public void runOpMode(){
     motorSetup();
+    imuSetup();
     waitForStart();
     operation();
     }
@@ -34,20 +39,42 @@ public class spencerBotTest extends LinearOpMode{
         leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
+
+    public void imuSetup(){
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
+    }
     public void drivePower(){
-        double translX = gamepad1.left_stick_x;
-        double translY = -gamepad1.left_stick_y;
-        double rot = gamepad1.right_stick_x;
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-        leftFront.setPower(translY + rot + translX);
-        leftBack.setPower(translY - rot + translX);
+        double rotIn = gamepad1.left_stick_x;
+        double translY = -gamepad1.left_stick_y * 1.1;
+        double translX = gamepad1.right_stick_x;
+        double scaleInsure = Math.max(Math.abs(translY) + Math.abs(rotIn) + Math.abs(translX), 1);
 
-        rightFront.setPower(translY - rot - translX);
-        rightBack.setPower(translY + rot - translX);
+        double rotTranslX = translX * Math.cos(-botHeading) - translY * Math.sin(-botHeading);
+        double rotTranslY = translX * Math.sin(-botHeading) + translY * Math.cos(-botHeading);
+
+        double leftFrontPower = (rotTranslY + rotIn + rotTranslX)/scaleInsure;
+        double leftBackPower = (rotTranslY - rotIn + rotTranslX)/scaleInsure;
+        double rightFrontPower = (rotTranslY - rotIn - rotTranslX)/scaleInsure;
+        double rightBackPower = (rotTranslY + rotIn - rotTranslX)/scaleInsure;
+
+        leftFront.setPower(leftFrontPower);
+        leftBack.setPower(leftBackPower);
+
+        rightFront.setPower(rightFrontPower);
+        rightBack.setPower(rightBackPower);
     }
     public void operation(){
         while(opModeIsActive()){
             drivePower();
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
             telemetry.addData("Running?", opModeIsActive());
             telemetry.update();
         }
